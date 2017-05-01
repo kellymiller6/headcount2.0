bb280dc8a5da473969c7df4dbcd16a9d7cb02e0e
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-01 17:39:58 -0600

    testing on iteration 1

--- src/helper.js
+++ src/helper.js
@@ -4,20 +4,33 @@ constructor(data){
   this.data = this.duplicateCleaner(data)
   }
 
+convertOrSetData(data){
+  if(isNaN(this.data.Data)){
+    this.data.Data === 0;
+  }
+  console.log(this.data.data)
+}
+
   duplicateCleaner(data){
     return data.reduce((acc, curVal) => {
       const district = curVal.Location;
       const year = curVal.TimeFrame;
-      const data = curVal.Data
+      const data = Math.round(curVal.Data * 1000)/1000;
 
       if (!acc[district]) {
-       acc[district] = { 'location': district, 'yearlyData': {}};
+       acc[district.toUpperCase()] = { 'location': district, 'data': {}};
       }
 
-      acc[district]['yearlyData'][year] = data;
+      acc[district.toUpperCase()]['data'][year] = data;
       return acc;
     }, {});
   }
+findByName(location){
+  if(location === ''){
+    return undefined;
+  }else{
+    return this.data[location.toUpperCase()]
+  }
 
-
+}
 }
