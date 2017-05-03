06aa8e877eaa2da463b56b8e04b1990a1e4fdd67
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-03 09:56:40 -0600

    musings

---
+++ src/Components/Cards.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export const Cards = ({ cards }) => {
+    return(
+      <div>
+        <div>
+          All the data
+          { cards }
+        </div>
+      </div>
+    )
+}
