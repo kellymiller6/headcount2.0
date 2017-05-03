279ea9dcec404c5bfbdbbe9cd7ecfaef12ea685f
Kelly Miller <kellyannemiller@gmail.com>
2017-05-03 10:36:48 -0600

    get data to render

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,14 +1,25 @@
-// import React, { Component } from 'react';
-//
-//
-// export class Card extends Component{
-//   render(){
-//     return(
-//       <div>
-//
-//         <h1 class = "school-name">School Name Will Go here</h1>
-//         <h2 class = "school-year">School Year will Go Here</h2>
-//       </div>
-//     )
-//   }
-// }
+import React from 'react';
+
+const Card = ({ location, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen }) => {
+  return (
+    <section>
+      <div>
+        <h4 >District: { location }</h4>
+        <p >2004: { four }</p>
+        <p >2005: { five }</p>
+        <p >2006: { six }</p>
+        <p >2007: { seven }</p>
+        <p >2008: { eight }</p>
+        <p >2009: { nine }</p>
+        <p >2010: { ten }</p>
+        <p >2011: { eleven }</p>
+        <p >2012: { twelve }</p>
+        <p >2013: { thirteen }</p>
+        <p >2014: { fourteen }</p>
+
+      </div>
+    </section>
+  );
+};
+
+export default Card;
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Card from './Card';
 
 const Cards = ({ districtData }) => {
     return(
@@ -9,7 +9,19 @@ const Cards = ({ districtData }) => {
         { Object.keys(districtData).map((district, index) => {
           return(
             <div key={index}>
-              { district }
+              <Card location={ district}
+                    four={ districtData[district].data[2004]}
+                    five={ districtData[district].data[2005]}
+                    six={ districtData[district].data[2006]}
+                    seven={ districtData[district].data[2007]}
+                    eight={ districtData[district].data[2008]}
+                    nine={ districtData[district].data[2009]}
+                    ten={ districtData[district].data[2010]}
+                    eleven={ districtData[district].data[2011]}
+                    twelve={ districtData[district].data[2012]}
+                    thirteen={ districtData[district].data[2013]}
+                    fourteen={ districtData[district].data[2014]}
+                    />
             </div>
           )
         })}
