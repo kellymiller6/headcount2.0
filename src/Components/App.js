887cc54fa9607b589454a47a62a4067999409e7f
Kelly Miller <kellyannemiller@gmail.com>
2017-05-03 09:41:55 -0600

    try to get data to render

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import DistrictRepository from '../helper.js';
 import '../App.css';
 import Controls from './Controls';
+import Cards from './Cards';
+
 import kinderData from '../../data/kindergartners_in_full_day_program';
 
 
@@ -15,16 +17,22 @@ class App extends Component {
 
   componentDidMount() {
     const district = new DistrictRepository(kinderData)
+    console.log(district.data)
     this.setState({
       districtData: district.data
     })
   }
+
+  // findSchools(userInput){
+  //   this.DistrictRepository.findByName(userInput);
+  // }
 
   render() {
     return (
       <div>
         <h1>Welcome To Headcount 2.0</h1>
         <Controls />
+        <Cards cards={this.state.districtData} />
       </div>
     );
   }
