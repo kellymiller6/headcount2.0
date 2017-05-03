d89b0884397741e199be46e1deb8850963e8daf0
Kelly Miller <kellyannemiller@gmail.com>
2017-05-02 20:39:24 -0600

    add component files

---
+++ src/Components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import DistrictRepository from './helper.js';
+import './App.css';
+import DistrictRepository from '../helper';
+import Controls from './Controls';
+import kinderData from '../../data/kindergartners_in_full_day_program';
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      districtData: {}
+    }
+  }
+
+  componentDidMount() {
+    const district = new DistrictRepository(kinderData)
+    this.setState({
+      districtData: district.data
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome To Headcount 2.0</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
