a98cd1fc7a0825d58a0a77d81c6de20186962d1d
Kelly Miller <kellyannemiller@gmail.com>
2017-05-03 09:13:39 -0600

    add controls to app

--- src/Components/App.js
+++ src/Components/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     return (
       <div>
         <h1>Welcome To Headcount 2.0</h1>
+        <Controls />
       </div>
     );
   }
---
+++ src/Components/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export class Card extends Component{
+  render(){
+    return(
+      <div>
+        <h1 class = "school-name">School Name Will Go here</h1>
+        <h2 class = "school-year">School Year will Go Here</h2>
+      </div>
+    )
+  }
+}
